ca6faaef524cbce06e8098a418305a93bd16ef85
casey langen <casey.langen@gmail.com>
2019-01-12 00:04:15 -0800

    Added commands to query and update monitors using basic shell commands an the pstream header.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -40,6 +40,13 @@
 #include <f8n/debug/debug.h>
 #include <f8n/environment/Environment.h>
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+#include "pstream.h"
+
 static const std::string APP_NAME = "xdimmer";
 static const int MAX_SIZE = 1000;
 static const int DEFAULT_WIDTH = 100;
@@ -48,6 +55,107 @@ static const int DEFAULT_HEIGHT = 26;
 static const int MIN_HEIGHT = 12;
 
 using namespace cursespp;
+
+namespace str {
+    std::string trim(const std::string &s) {
+        /* so lazy https://stackoverflow.com/a/17976541 */
+        auto front = std::find_if_not(s.begin(), s.end(), isspace);
+        auto back = std::find_if_not(s.rbegin(), s.rend(), isspace).base();
+        return (back <= front ? std::string() : std::string(front, back));
+    }
+
+    std::vector<std::string> split(const std::string& str, const std::string& delimiters) {
+        using ContainerT = std::vector<std::string>;
+        ContainerT tokens;
+        std::string::size_type pos, lastPos = 0, length = str.length();
+
+        using value_type = typename ContainerT::value_type;
+        using size_type = typename ContainerT::size_type;
+
+        while (lastPos < length + 1) {
+            pos = str.find_first_of(delimiters, lastPos);
+            if (pos == std::string::npos) {
+                pos = length;
+            }
+
+            if (pos != lastPos) {
+                std::string token = trim(value_type(
+                    str.data() + lastPos, (size_type) pos - lastPos));
+
+                if (token.size()) {
+                    tokens.push_back(token);
+                }
+            }
+
+            lastPos = pos + 1;
+        }
+
+        return tokens;
+    }
+
+    template<typename... Args>
+    static std::string fmt(const std::string& format, Args ... args) {
+        size_t size = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; /* extra space for '\0' */
+        std::unique_ptr<char[]> buf(new char[size]);
+        std::snprintf(buf.get(), size, format.c_str(), args ...);
+        return std::string(buf.get(), buf.get() + size - 1); /* omit the '\0' */
+    }
+}
+
+struct Monitor {
+    const std::string name;
+    const float brightness;
+};
+
+namespace cmd {
+
+    std::vector<std::string> queryNames() {
+        std::vector<std::string> names;
+        redi::ipstream in("xrandr -q | grep \" connected \"");
+        for (std::string line; std::getline(in, line);) {
+            auto parts = str::split(line, " ");
+            if (parts.size()) {
+                //std::cout << "name: " << parts[0] << "\n";
+                names.push_back(parts[0]);
+            }
+        }
+        return names;
+    }
+
+    std::vector<float> queryValues() {
+        std::vector<float> values;
+        redi::ipstream in("xrandr --verbose | grep -i brightness");
+        for (std::string line; std::getline(in, line);) {
+            auto parts = str::split(line, " ");
+            if (parts.size() > 1) {
+                //std::cout << "value: " << parts[1] << "\n";
+                values.push_back(std::stof(parts[1]));
+            }
+        }
+
+        return values;
+    }
+
+    std::vector<Monitor> query() {
+        auto names = queryNames();
+        auto values = queryValues();
+        assert(names.size() == values.size());
+        std::vector<Monitor> monitors;
+        for (size_t i = 0; i < names.size(); i++) {
+            monitors.push_back(Monitor{names[i], values[i]});
+        }
+        return monitors;
+    }
+
+    void update(const Monitor& monitor, float brightness) {
+        std::string command = str::fmt(
+            "xrandr --output %s --brightness %f\n",
+            monitor.name.c_str(),
+            brightness);
+        //std::cout << command;
+        redi::opstream out(command);
+    }
+}
 
 class MainLayout: public LayoutBase {
     public:
@@ -94,7 +202,11 @@ int main(int argc, char* argv[]) {
         return false;
     });
 
-    app.Run(std::make_shared<MainLayout>());
+    //app.Run(std::make_shared<MainLayout>());
+    auto monitors = cmd::query();
+    for (auto& m : monitors) {
+        cmd::update(m, m.brightness == 1.0 ? 0.75 : 1.0);
+    }
 
     f8n::debug::Stop();
 
